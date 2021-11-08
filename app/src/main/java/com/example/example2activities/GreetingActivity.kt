0cf09f542779bd66470227883713957f670fc878
Prades <alupra4265@ieselcaminas.org>
2021-11-08 18:29:05 +0100

    Initial commit

---
+++ app/src/main/java/com/example/example2activities/GreetingActivity.kt
@@ -0,0 +1,22 @@
+package com.example.example2activities
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class GreetingActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.greeting_layout)
+
+        val intent = intent
+        val b = intent.extras
+
+        val name = b?.getString("name")
+
+        val text = findViewById<TextView>(R.id.textViewGreeting)
+        text.text = "Hello $name"
+    }
+}
---
+++ app/src/main/java/com/example/example2activities/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.example2activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            val edit = findViewById<EditText>(R.id.editTextName)
+            val name = edit.text.toString()
+            val intent = Intent(applicationContext, GreetingActivity::class.java)
+            intent.putExtra("name",name)
+            startActivity(intent)
+        }
+    }
+}
